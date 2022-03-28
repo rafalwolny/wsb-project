bcaac53391470692fd0ccdc5574a04c9e43daeee
Rafal Wolny <rafal.wolny96@gmail.com>
2022-03-28 20:59:10 +0200

    add basic features

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const mysql = require('mysql');
 const app = express();
 
 app.use(cors());
+// app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const connection = mysql.createPool({
   connectionLimit: 20,
@@ -14,20 +16,50 @@ const connection = mysql.createPool({
 });
 
 app.get('/', (req, res) => {
-  connection.query('SELECT * FROM books', (err, rows) => {
+  const query = "SELECT * FROM books;";
+  connection.query(query, (err, rows) => {
     if (err) {
       res.json({
-        success: false,
-        err
+        success: false, err
       });
     }
     else {
       res.json({
-        success: true,
-        rows
+        success: true, rows
       });
     }
   });
+});
+
+app.get('/search_book', (req, res) => {
+  const searchValue = `%${req.body.book_title}%`;
+  const query = "SELECT * FROM books WHERE book_title LIKE ?;";
+  connection.query(query, searchValue, (err, rows) => {
+    if (err) {
+      res.json({
+        success: false, err
+      });
+    }
+    else {
+      res.json({
+        success: true, rows
+      });
+    }
+  });
+});
+
+app.post("/add_book", (req, res) => {
+  const query = "INSERT INTO books(book_title, book_author, book_year) VALUES(?, ?, ?);";
+  connection.query(
+    query, 
+    [req.body.book_title, req.body.book_author, req.body.book_year], 
+    (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else res.status(200).send("request delivered");
+    }
+  ); 
 });
 
 app.listen(3000, () => console.log('listining on port 3000'));
