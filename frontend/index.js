9ba46457bb859c27a4579803c276638c4dcba1d4
Rafal Wolny <rafal.wolny96@gmail.com>
2022-03-30 22:28:33 +0200

    add initial communication between frontend and backend

--- frontend/index.js
+++ frontend/index.js
@@ -1,15 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
+const loadTable = (result) => {
+  const table = document.querySelector(".table-body");
+  const notFoundResponse = `<tr><td class="table-mnzm">Book not found</td></tr>`;
+  if (result.length == 0) {
+    table.innerHTML = notFoundResponse;
+    return;
+  }
+
+  let html = "";
+  const tableLength = result.length >= 10 ? 10 : result.length;
+
+  for(i = 0; i < tableLength; i++) {
+    html += `<tr>`;
+    html += `<td class="table-mnzm">${result[i].book_title}</td>`;
+    html += `<td class="table-mnzm">${result[i].book_author}</td>`;
+    html += `<td class="table-mnzm">${result[i].book_year}</td>`;
+    html += `<td class="table-mnzm"><button id="button-delete" class="button" type="button">Delete</button></td>`;
+    html += `</tr>`;
+  }
+
+  table.innerHTML = html;
+}
+
+const searchAll = () => {
   fetch("http://localhost:3000/search_all")
     .then(response => response.json())
-    .then(data => console.log(data.rows))
-});
+    .then(data => loadTable(data.result))
+}
 
-const searchButton = document.querySelector(".search-btn");
+document.addEventListener("DOMContentLoaded", searchAll());
+
+const searchButton = document.querySelector("#button-search");
 
 searchButton.onclick = () => {
-  const searchValue = document.querySelector('.search-text').value;
-  // fetch("http://localhost:3000/search_book", searchValue)
-  //   .then(response => response.json())
-  //   .then(data => console.log(data.rows))
-    
+  const searchValue = document.querySelector(".search-text").value;
+  if (searchValue) {
+    fetch("http://localhost:3000/search_book/" + searchValue)
+      .then(response => response.json())
+      .then(data => loadTable(data.result))
+  } else searchAll();
 }
