9f42bc3d280f986ab93516b06fd5176fe78ab860
Rafal Wolny <rafal.wolny96@gmail.com>
2022-04-02 09:44:26 +0200

    modify user data and add docker-compose for aws

--- cdk/lib/backend-stack.ts
+++ cdk/lib/backend-stack.ts
@@ -69,8 +69,18 @@ export class BackendStack extends Stack {
       "sudo amazon-linux-extras install docker",
       "sudo service docker start",
       "sudo systemctl enable docker",
+      // install docker-compose
+      "sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+      // add permissions for ssm-user
       "sudo usermod -a -G docker ssm-user",
     ];
+
+    // // run docker-compose
+    // const backendUserData = [
+    //   "cd",
+    //   "docker-compose -f"
+    // ]
 
     backendAutoScalingGroup.addUserData(...baseUserData);
     frontendAutoScalingGroup.addUserData(...baseUserData);
